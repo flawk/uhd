d1fcf30f47b097b98aa085f999c4016374824ab8
Joe <joe.melsha@live.com>
2022-04-19 13:31:13 -0500

    multi_usrp: Fix type specialization error with sensor intrinsics (typo)

--- host/include/uhd/usrp/multi_usrp.hpp
+++ host/include/uhd/usrp/multi_usrp.hpp
@@ -1972,7 +1972,7 @@ public:
         if constexpr (L == sensor_location_t::MBOARD) {
             if (index == ALL_MBOARDS) {
                 for (size_t mboard = 0; mboard < get_num_mboards(); ++mboard) {
-                    if (get_sensor<T, L>(name, mboard).is_value(expected_value)) {
+                    if (get_sensor<L, T>(name, mboard).is_value(expected_value)) {
                         return false;
                     }
                 }
@@ -1981,7 +1981,7 @@ public:
         } else if constexpr (L == sensor_location_t::RX) {
             if (index == ALL_CHANS) {
                 for (size_t ch = 0; ch < get_rx_num_channels(); ++ch) {
-                    if (get_sensor<T, L>(name, ch).is_value(expected_value)) {
+                    if (get_sensor<L, T>(name, ch).is_value(expected_value)) {
                         return false;
                     }
                 }
@@ -1990,7 +1990,7 @@ public:
         } else if constexpr (L == sensor_location_t::TX) {
             if (index == ALL_CHANS) {
                 for (size_t ch = 0; ch < get_tx_num_channels(); ++ch) {
-                    if (get_sensor<T, L>(name, ch).is_value(expected_value)) {
+                    if (get_sensor<L, T>(name, ch).is_value(expected_value)) {
                         return false;
                     }
                 }
@@ -1999,7 +1999,7 @@ public:
         } else {
             static_assert(dependent_false<L>::value, "Unknown sensor location");
         }
-        return get_sensor<T, L>(name, index).is_value(expected_value);
+        return get_sensor<L, T>(name, index).is_value(expected_value);
     }
 
     template <sensor_location_t L>
