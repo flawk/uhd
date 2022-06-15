aa2b5c08298275be31fd6cf910f3fbf86d2c2a10
jrmelsha <joe@flawk.com>
2022-06-14 19:37:53 -0500

    time_spec: convert to header-only

--- host/include/uhd/types/time_spec.hpp
+++ host/include/uhd/types/time_spec.hpp
@@ -11,6 +11,10 @@
 #include <stdint.h>
 #include <boost/operators.hpp>
 #include <ostream>
+#include <cmath>
+#include <sstream>
+#include <iomanip>
+#include <limits>
 
 namespace uhd {
 
@@ -26,26 +30,62 @@ namespace uhd {
  * This gives the fractional seconds enough precision to unambiguously
  * specify a clock-tick/sample-count up to rates of several petahertz.
  */
-class UHD_API time_spec_t : boost::additive<time_spec_t>,
-                            boost::additive<time_spec_t, double>,
-                            boost::totally_ordered<time_spec_t>
-{
+class UHD_API time_spec_t {
+private:
+    /// Time spec constructor
+    #define time_spec_init(full, frac)                         \
+    {                                                          \
+        const int64_t _full     = int64_t(full);               \
+        const double _frac      = double(frac);                \
+        const int64_t _frac_int = static_cast<int64_t>(_frac); \
+        _full_secs              = _full + _frac_int;           \
+        _frac_secs              = _frac - _frac_int;           \
+        if (_frac_secs < 0) {                                  \
+            _full_secs -= 1;                                   \
+            _frac_secs += 1;                                   \
+        }                                                      \
+    }
+
+    /// Helpers
+    static constexpr int64_t fast_llround(double x) noexcept
+    {
+        return static_cast<int64_t>(x + 0.5); // assumption of non-negativity
+    }
+
+
+    /// private time storage details
+    int64_t _full_secs{};
+    double _frac_secs{};
+
 public:
     // A special value that signifies immediate execution
     static constexpr double ASAP = 0.0;
 
     /*!
+     * Create a time_spec_t with 0 time
+     */
+    /*constexpr time_spec_t() noexcept
+    {
+    }*/
+
+    /*!
      * Create a time_spec_t from a real-valued seconds count.
      * \param secs the real-valued seconds count (default = 0)
      */
-    time_spec_t(double secs = 0);
+    constexpr time_spec_t(double secs = 0) noexcept
+    {
+        time_spec_init(0, secs);
+    }
 
     /*!
      * Create a time_spec_t from whole and fractional seconds.
      * \param full_secs the whole/integer seconds count
      * \param frac_secs the fractional seconds count (default = 0)
      */
-    time_spec_t(int64_t full_secs, double frac_secs = 0);
+    constexpr time_spec_t(int64_t full_secs, double frac_secs = 0) noexcept
+    {
+        time_spec_init(full_secs, frac_secs);
+    }
 
     /*!
      * Create a time_spec_t from whole seconds and fractional ticks.
@@ -54,7 +94,11 @@ public:
      * \param tick_count the fractional seconds tick count
      * \param tick_rate the number of ticks per second
      */
-    time_spec_t(int64_t full_secs, long tick_count, double tick_rate);
+    constexpr time_spec_t(int64_t full_secs, int64_t tick_count, double tick_rate) noexcept
+    {
+        const double frac_secs = tick_count / tick_rate;
+        time_spec_init(full_secs, frac_secs);
+    }
 
     /*!
      * Create a time_spec_t from a 64-bit tick count.
@@ -62,7 +106,15 @@ public:
      * \param ticks an integer count of ticks
      * \param tick_rate the number of ticks per second
      */
-    static time_spec_t from_ticks(long long ticks, double tick_rate);
+    constexpr static time_spec_t from_ticks(int64_t ticks, double tick_rate) noexcept
+    {
+        const int64_t rate_i      = static_cast<int64_t>(tick_rate);
+        const double rate_f       = tick_rate - rate_i;
+        const int64_t secs_full   = int64_t(ticks / rate_i);
+        const int64_t ticks_error = ticks - (secs_full * rate_i);
+        const double ticks_frac   = ticks_error - secs_full * rate_f;
+        return time_spec_t(secs_full, ticks_frac / tick_rate);
+    }
 
     /*!
      * Convert the fractional seconds to clock ticks.
@@ -70,7 +122,10 @@ public:
      * \param tick_rate the number of ticks per second
      * \return the fractional seconds tick count
      */
-    long get_tick_count(double tick_rate) const;
+    constexpr int64_t get_tick_count(double tick_rate) const noexcept
+    {
+        return fast_llround(this->get_frac_secs() * tick_rate);
+    }
 
     /*!
      * Convert the time spec into a 64-bit tick count.
@@ -78,7 +133,16 @@ public:
      * \param tick_rate the number of ticks per second
      * \return an integer number of ticks
      */
-    long long to_ticks(const double tick_rate) const;
+    constexpr int64_t to_ticks(const double tick_rate) const noexcept
+    {
+        const int64_t rate_i     = static_cast<int64_t>(tick_rate);
+        const double rate_f      = tick_rate - rate_i;
+        const int64_t ticks_full = this->get_full_secs() * rate_i;
+        const double ticks_error = this->get_full_secs() * rate_f;
+        const double ticks_frac  = this->get_frac_secs() * tick_rate;
+        return ticks_full + fast_llround(ticks_error + ticks_frac);
+    }
+
 
     /*!
      * Get the time as a real-valued seconds count.
@@ -86,52 +150,142 @@ public:
      * the precision of the fractional seconds may be lost.
      * \return the real-valued seconds
      */
-    double get_real_secs(void) const;
+    constexpr double get_real_secs(void) const noexcept
+    {
+        return this->get_full_secs() + this->get_frac_secs();
+    }
 
     /*!
      * Get the whole/integer part of the time in seconds.
      * \return the whole/integer seconds
      */
-    int64_t get_full_secs(void) const;
+    constexpr int64_t get_full_secs(void) const noexcept
+    {
+        return _full_secs;
+    }
 
     /*!
      * Get the fractional part of the time in seconds.
      * \return the fractional seconds
      */
-    double get_frac_secs(void) const;
+    constexpr double get_frac_secs(void) const noexcept
+    {
+        return _frac_secs;
+    }
 
     //! stringify, keeping full precision
-    operator std::string() const;
+    operator std::string() const
+    {
+        std::stringstream ss;
+        ss << this;
+        return ss.str();
+    }
 
     //! Implement addable interface
-    time_spec_t& operator+=(const time_spec_t&);
-    time_spec_t& operator+=(double&);
+    friend constexpr time_spec_t operator+(const time_spec_t& l, const time_spec_t& r)
+    {
+        return {
+            l.get_full_secs() + r.get_full_secs(), l.get_frac_secs() + r.get_frac_secs()};
+    }
+    time_spec_t& operator+=(const time_spec_t& rhs) noexcept
+    {
+        time_spec_init(this->get_full_secs() + rhs.get_full_secs(),
+            this->get_frac_secs() + rhs.get_frac_secs());
+        return *this;
+    }
+    friend constexpr time_spec_t operator+(const time_spec_t& l, const double& r)
+    {
+        double full_secs = std::trunc(r);
+        return {l.get_full_secs() + static_cast<int64_t>(full_secs),
+            l.get_frac_secs() + (r - full_secs)};
+    }
+    time_spec_t& operator+=(const double& rhs) noexcept
+    {
+        double full_secs = std::trunc(rhs);
+        time_spec_init(this->get_full_secs() + static_cast<int64_t>(full_secs),
+            this->get_frac_secs() + (rhs - full_secs));
+        return *this;
+    }
     //! Implement subtractable interface
-    time_spec_t& operator-=(const time_spec_t&);
-    time_spec_t& operator-=(double&);
+    friend constexpr time_spec_t operator-(const time_spec_t& l, const time_spec_t& r)
+    {
+        return {
+            l.get_full_secs() - r.get_full_secs(), l.get_frac_secs() - r.get_frac_secs()};
+    }
+    time_spec_t& operator-=(const time_spec_t& rhs) noexcept
+    {
+        time_spec_init(this->get_full_secs() - rhs.get_full_secs(),
+            this->get_frac_secs() - rhs.get_frac_secs());
+        return *this;
+    }
+    friend constexpr time_spec_t operator-(const time_spec_t& l, const double& r)
+    {
+        double full_secs = std::trunc(r);
+        return {l.get_full_secs() - static_cast<int64_t>(full_secs),
+            l.get_frac_secs() - (r - full_secs)};
+    }
+    time_spec_t& operator-=(const double& rhs) noexcept
+    {
+        double full_secs = std::trunc(rhs);
+        time_spec_init(this->get_full_secs() - static_cast<int64_t>(full_secs),
+            this->get_frac_secs() - (rhs - full_secs));
+        return *this;
+    }
 
-    // private time storage details
-private:
-    int64_t _full_secs;
-    double _frac_secs;
+    //! Implement equality_comparable interface
+    friend constexpr bool operator==(const time_spec_t& l, const time_spec_t& r)
+    {
+        return l.get_full_secs() == r.get_full_secs()
+               && l.get_frac_secs() == r.get_frac_secs();
+    }
+
+    //! Implement inequality_comparable interface
+    friend constexpr bool operator!=(const time_spec_t& l, const time_spec_t& r)
+    {
+        return l.get_full_secs() != r.get_full_secs()
+               || l.get_frac_secs() != r.get_frac_secs();
+    }
+
+    //! Implement less_than_comparable interface
+    friend constexpr bool operator<(const time_spec_t& l, const time_spec_t& r)
+    {
+        return l.get_full_secs() < r.get_full_secs()
+               || (l.get_full_secs() == r.get_full_secs()
+                   && l.get_frac_secs() < r.get_frac_secs());
+    }
+
+    //! Implement greater_than_comparable interface
+    friend constexpr bool operator>(const time_spec_t& l, const time_spec_t& r)
+    {
+        return l.get_full_secs() > r.get_full_secs()
+               || (l.get_full_secs() == r.get_full_secs()
+                   && l.get_frac_secs() > r.get_frac_secs());
+    }
+
+    //! Implement less_than_or_equal_comparable interface
+    friend constexpr bool operator<=(const time_spec_t& l, const time_spec_t& r)
+    {
+        return l.get_full_secs() < r.get_full_secs()
+               || (l.get_full_secs() == r.get_full_secs()
+                   && l.get_frac_secs() <= r.get_frac_secs());
+    }
+
+    //! Implement greater_than_or_equal_comparable interface
+    friend constexpr bool operator>=(const time_spec_t& l, const time_spec_t& r)
+    {
+        return l.get_full_secs() > r.get_full_secs()
+               || (l.get_full_secs() == r.get_full_secs()
+                   && l.get_frac_secs() >= r.get_frac_secs());
+    }
+
+    //! Implement ostream interface
+    friend std::ostream& operator<<(std::ostream& os, const uhd::time_spec_t& t)
+    {
+        std::stringstream ss_frac;
+        ss_frac << std::setprecision(std::numeric_limits<double>::digits10 + 1)
+                << std::showpoint << t.get_frac_secs();
+        return os << t.get_full_secs() << ss_frac.str().substr(1);
+    }
 };
 
-//! Implement equality_comparable interface
-UHD_API bool operator==(const time_spec_t&, const time_spec_t&);
-
-//! Implement less_than_comparable interface
-UHD_API bool operator<(const time_spec_t&, const time_spec_t&);
-
-UHD_INLINE int64_t time_spec_t::get_full_secs(void) const
-{
-    return this->_full_secs;
-}
-
-UHD_INLINE double time_spec_t::get_frac_secs(void) const
-{
-    return this->_frac_secs;
-}
-
 } // namespace uhd
-
-UHD_API std::ostream& operator<<(std::ostream& os, const uhd::time_spec_t& t);
